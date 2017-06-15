c8b2303f9917fa0965425c8a376aa99b6a4869e8
elkuku <der.el.kuku@gmail.com>
2017-06-15 18:01:25 -0500

    Use abs path

--- pingtest.sh
+++ pingtest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ROOT=`realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
+
 test=$(ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' | cut -d '/' -f 2)
 
-echo $(date +"%Y%m%d%H%M") $test >> pingtest$(date +"%Y%m%d").txt
+echo $(date +"%Y%m%d%H%M") ${test} >> ${ROOT}/pingtest$(date +"%Y%m%d").txt
