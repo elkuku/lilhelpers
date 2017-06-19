0eae14080b8a650c7e53ccda14156d3c0e23a308
elkuku <der.el.kuku@gmail.com>
2017-06-19 12:40:41 -0500

    Add another ping test

--- pingtest.sh
+++ pingtest.sh
@@ -2,6 +2,7 @@
 
 ROOT=`realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
 
-test=$(ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' | cut -d '/' -f 2)
+test=$(ping -c 4 google.com | tail -1| awk '{print $4}' | cut -d '/' -f 2)
+test2=$(ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' | cut -d '/' -f 2)
 
-echo $(date +"%Y%m%d%H%M") ${test} >> ${ROOT}/pingtest$(date +"%Y%m%d").txt
+echo $(date +"%Y%m%d%H%M") ${test} ${test2} >> ${ROOT}/pingtest$(date +"%Y%m%d").txt
